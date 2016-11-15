ccf54114c63b622d3d9e1cb63a746aa05aa35f3b
CodeforLiberty <sswell@live.com>
2016-11-15 15:21:05 -0500

    Promises

--- spotify.js
+++ spotify.js
@@ -19,6 +19,11 @@ var getFromApi = function(endpoint, query) {
 
 };
 
+// What is a promise?
+// - Object.
+// - { then: function(), catch: function() }
+// - { sayHello: function() }
+
 
 var artist;
 console.log(1);
@@ -28,14 +33,23 @@ var getArtist = function(name) {
       limit:1,
       type: 'artist'
     };
-    console.log(3);
-    getFromApi('search', param).then(function(json){ //json is  promise.value
-      console.log(7);
-      // artist = item.artists.items[0];
-      return artist;
-    });
-    console.log(4.5);
 
+    return getFromApi('search', param).then(function (json) {  
+        artist = json.artists.items[0];
+        return artist;
+    });
+
+    console.log(3);
+   // getFromApi('search', param).then(function(json){ //json is  promise.value
+      console.log(7);
+      
+    console.log(json.artists.items);
+    
+  //}).catch(function(err) {
+    console.log(err);
+//});
+    console.log(4.5);
+    //return getFromApi()
   };
-console.log(2);
-getArtist('green day');
+//console.log(2);
+
