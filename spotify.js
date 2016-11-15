a817a2a73a4ac590b03c5990b7b7560bb100bd33
iamromanh <romanh99@gmail.com>
2016-11-15 13:16:59 -0500

    added break down via console.log for promises

--- spotify.js
+++ spotify.js
@@ -1,23 +1,41 @@
+console.log(0);
 var getFromApi = function(endpoint, query) {
-    var url = 'https://api.spotify.com/v1/' + endpoint;
+    console.log(4.1);
+    var url = 'https://api.spotify.com/v1/' + endpoint; //'https://api.spotify.com/v1/search?foo=bar&a=b'
 
     var queryString = Qs.stringify(query);
     if (queryString) {
         url += '?' + queryString;
-    };
-
+    }
+    console.log(4.2);
     return fetch(url).then(function(response) {
+        console.log(5);
         if (response.status < 200 || response.status >= 300) {
             return Promise.reject(response.statusText);
         }
-        return response.json();
+        console.log(6);
+        return response.json(); //returns a promise with status and value
     });
+
 };
 
 
 var artist;
+console.log(1);
 var getArtist = function(name) {
-    let search = { q:name, limit:1, type: 'artist'};
-    getFromApi('https://api.spotify.com/v1/', search )
-    .then(function(name)  { artist = item.artists.items[0]; return artist;})
+    var param = {
+      q:name,
+      limit:1,
+      type: 'artist'
     };
+    console.log(3);
+    getFromApi('search', param).then(function(json){ //json is  promise.value
+      console.log(7);
+      // artist = item.artists.items[0];
+      return artist;
+    });
+    console.log(4.5);
+
+  };
+console.log(2);
+getArtist('green day');
