d1197b844c7668048a1970f7ab2b2cff5603b05c
iamromanh <romanh99@gmail.com>
2016-11-16 00:13:57 -0500

    added Promises in parallel

--- spotify.js
+++ spotify.js
@@ -1,22 +1,17 @@
-console.log(0);
 var getFromApi = function(endpoint, query) {
-    console.log(4.1);
     var url = 'https://api.spotify.com/v1/' + endpoint; //'https://api.spotify.com/v1/search?foo=bar&a=b'
 
     var queryString = Qs.stringify(query);
     if (queryString) {
         url += '?' + queryString;
     }
-    console.log(4.2);
+
     return fetch(url).then(function(response) {
-        console.log(5);
         if (response.status < 200 || response.status >= 300) {
             return Promise.reject(response.statusText);
         }
-        console.log(6);
         return response.json(); //returns a promise with status and value
     });
-
 };
 
 // What is a promise?
@@ -26,49 +21,28 @@ var getFromApi = function(endpoint, query) {
 
 
 var artist;
-console.log(1);
 var getArtist = function(name) {
     var param = {
       q:name,
       limit:1,
       type: 'artist'
-      // id: '1uNFoZAHBGtllmzznpCI3s'
     };
 
     return getFromApi('search', param).then(function (json) {
         artist = json.artists.items[0];
-        console.log(json.artists.items[0].id);
         var artistId = json.artists.items[0].id;
-       
-        // }); 
-         // var getRelatedArtist = (function(id){
-         var url = `artists/${artistId}/related-artists`;
-          
-         //  // get the artist ID from the initial getFromApi function and hook it
-         //  var artistId = json.artists.items[0].id;
-         //  console.log(artistId);
-          // into this function
-    //     return getRelatedArtist()
-    // }).then(function(artists){
-          // Set artist.related to item.artists
-          // Then return artist object i.e return artist;
-          // getRelatedArtist();
+        var url = `artists/${artistId}/related-artists`;
 
-          return getFromApi(url);
-        }).then(function(item) {
-            console.log(item);
-            artist.related=item.artists;
+        var firstPromise = getFromApi(url);
+        var secondPromise = getFromApi(url);
+        var thirdPromise = getFromApi(url);
+
+          return Promise.all([firstPromise, secondPromise, thirdPromise]);
+        })allPromise.then(function(promiseResults) {
+          console.log(promiseResults);
+            artist.related=promiseResults[0].artists;
             return artist;
         })
     .catch(function(err) {
-      console.log("what???");
     });
-  }
-
-
-getArtist('Justin');
-// getRelatedArtist(artistId);
-
-// asyncthing.then(
-//     return something).
-//     then()
+  };
