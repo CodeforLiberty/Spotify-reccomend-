1838a709b9211af3c70f51aa581374952cef691a
CodeforLiberty <sswell@live.com>
2016-12-13 10:37:30 -0500

    New version of spotify promise exercises

--- spotify.js
+++ spotify.js
@@ -1,74 +1,36 @@
-console.log(0);
 var getFromApi = function(endpoint, query) {
-    console.log(4.1);
-    var url = 'https://api.spotify.com/v1/' + endpoint; //'https://api.spotify.com/v1/search?foo=bar&a=b'
+    var url = 'https://api.spotify.com/v1/' + endpoint;
 
     var queryString = Qs.stringify(query);
     if (queryString) {
         url += '?' + queryString;
     }
-    console.log(4.2);
+
     return fetch(url).then(function(response) {
-        console.log(5);
         if (response.status < 200 || response.status >= 300) {
             return Promise.reject(response.statusText);
         }
-        console.log(6);
-        return response.json(); //returns a promise with status and value
+        return response.json();
     });
-
 };
-
-// What is a promise?
-// - Object.
-// - { then: function(), catch: function() }
-// - { sayHello: function() }
 
 
 var artist;
-console.log(1);
 var getArtist = function(name) {
-    var param = {
-      q:name,
-      limit:1,
-      type: 'artist'
-      // id: '1uNFoZAHBGtllmzznpCI3s'
+    // Edit me!
+    const query = {
+        q:name,
+        limit:1,
+        type:'artist'
     };
 
-    return getFromApi('search', param).then(function (json) {
-        artist = json.artists.items[0];
-        console.log(json.artists.items[0].id);
-        var artistId = json.artists.items[0].id;
-       
-        // }); 
-         // var getRelatedArtist = (function(id){
-         var url = `artists/${artistId}/related-artists`;
-          
-         //  // get the artist ID from the initial getFromApi function and hook it
-         //  var artistId = json.artists.items[0].id;
-         //  console.log(artistId);
-          // into this function
-    //     return getRelatedArtist()
-    // }).then(function(artists){
-          // Set artist.related to item.artists
-          // Then return artist object i.e return artist;
-          // getRelatedArtist();
+    
+   return getFromApi('search',query).then((response) => {
+    artist = response.artists.items[0];
+    return artist;
 
-          return getFromApi(url);
-        }).then(function(item) {
-            console.log(item);
-            artist.related=item.artists;
-            return artist;
-        })
-    .catch(function(err) {
-      console.log("what???");
+        console.log(response);
+    }).catch((err) => {
+        console.log("Not getting response");
     });
-  }
-
-
-getArtist('Justin');
-// getRelatedArtist(artistId);
-
-// asyncthing.then(
-//     return something).
-//     then()
+   };
