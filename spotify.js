6ff4988264e88378205ab44649572d7920956d3c
CodeforLiberty <sswell@live.com>
2016-12-13 11:08:34 -0500

    Converted to ES6 and formatted

--- spotify.js
+++ spotify.js
@@ -1,12 +1,12 @@
-var getFromApi = function(endpoint, query) {
-    var url = 'https://api.spotify.com/v1/' + endpoint;
+var getFromApi = (endpoint='search', query={q:'Twiztid',limit:1,type:'artist'}) => {
+    let url = 'https://api.spotify.com/v1/' + endpoint;
 
-    var queryString = Qs.stringify(query);
+    let queryString = Qs.stringify(query);
     if (queryString) {
         url += '?' + queryString;
     }
 
-    return fetch(url).then(function(response) {
+    return fetch(url).then((response) => {
         if (response.status < 200 || response.status >= 300) {
             return Promise.reject(response.statusText);
         }
@@ -16,8 +16,7 @@ var getFromApi = function(endpoint, query) {
 
 
 var artist;
-var getArtist = function(name) {
-    // Edit me!
+const getArtist = (name) => {
     const query = {
         q:name,
         limit:1,
@@ -27,10 +26,11 @@ var getArtist = function(name) {
     
    return getFromApi('search',query).then((response) => {
     artist = response.artists.items[0];
+    console.log(artist);
     return artist;
 
         console.log(response);
     }).catch((err) => {
-        console.log("Not getting response");
+        console.log("Not getting a response, did you type in a artist?");
     });
    };
